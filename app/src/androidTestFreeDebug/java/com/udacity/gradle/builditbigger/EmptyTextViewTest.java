11f2646de390aaf423c92629eee88a30c6a52869
nal3x <nalex@aegean.gr>
2018-07-06 23:53:08 +0300

    separate test for free version

---
+++ app/src/androidTestFreeDebug/java/com/udacity/gradle/builditbigger/EmptyTextViewTest.java
@@ -0,0 +1,56 @@
+package com.udacity.gradle.builditbigger;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+@RunWith(AndroidJUnit4.class)
+public class EmptyTextViewTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void clickTestJokeButton_fillsJokeTextView() {
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView((withId(R.id.tellJokeButton))).perform(click());
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction imageButton = onView(
+                allOf(withContentDescription("Interstitial close button"), isDisplayed()));
+        imageButton.perform(click());
+
+        onView(withId(R.id.joke_textview)).check(matches(not(withText(""))));
+    }
+
+
+}
+
